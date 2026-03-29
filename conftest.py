8b85f02f49fe65f4ecdc57007d3f693d5f029e51
Marcin Wierzbinski <marcin119a@gmail.com>
2026-03-29 13:17:17 +0200

    Dockerfile

--- conftest.py
+++ conftest.py
@@ -8,8 +8,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-BASE_URL = "http://localhost:5173"
-AUTH_URL = "http://localhost:8000"
+BASE_URL = os.getenv("BASE_URL", "http://localhost:5173")
+AUTH_URL = os.getenv("AUTH_URL", "http://localhost:8000")
 
 
 @pytest.fixture(scope="session")
@@ -28,13 +28,23 @@ def existing_user():
 @pytest.fixture(scope="function")
 def driver():
     options = Options()
-    #options.add_argument("--headless=new")
+    if os.getenv("CHROME_HEADLESS", "0") in {"1", "true", "TRUE", "yes", "YES"}:
+        options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1280,900")
     options.add_argument("--window-position=100,100")
+    chrome_bin = os.getenv("CHROME_BIN")
+    if chrome_bin:
+        options.binary_location = chrome_bin
 
-    service = Service(ChromeDriverManager().install())
+    chromedriver_path = os.getenv("CHROMEDRIVER_PATH")
+    if chromedriver_path and os.path.exists(chromedriver_path):
+        service = Service(chromedriver_path)
+    elif os.path.exists("/usr/bin/chromedriver"):
+        service = Service("/usr/bin/chromedriver")
+    else:
+        service = Service(ChromeDriverManager().install())
     drv = webdriver.Chrome(service=service, options=options)
     drv.implicitly_wait(8)
     yield drv
